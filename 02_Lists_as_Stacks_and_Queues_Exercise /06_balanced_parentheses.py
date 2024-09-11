91db123c28660cde27fcb92a341ce45bd14d641e
pgnikolov <pgnikolov@gmail.com>
2024-09-11 06:57:25 +0200

    lists-as-stacks-and-queues

---
+++ 02_Lists_as_Stacks_and_Queues_Exercise /06_balanced_parentheses.py
@@ -0,0 +1,22 @@
+parentheses_input = input()
+stack = []
+for symbol in parentheses_input:
+    if symbol in ['[', '{', '(']:
+        stack.append(symbol)
+    else:
+        if not stack:
+            print("NO")
+            break
+        if symbol == ']' and stack[-1] != '[':
+            print("NO")
+            break
+        elif symbol == '}' and stack[-1] != '{':
+            print("NO")
+            break
+        elif symbol == ')' and stack[-1] != '(':
+            print("NO")
+            break
+        else:
+            stack.pop()
+else:
+    print("YES")
