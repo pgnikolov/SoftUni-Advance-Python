6a09f4615530fbb3254629fe970ca1d1e5ffb746
pgnikolov <pgnikolov@gmail.com>
2024-11-30 15:16:42 +0100

    OOP - full score

--- Python-OOP-Exams/OOP_Exam_22_Aug_2020-Retake/unit_testing/project/test/test.py
+++ Python-OOP-Exams/OOP_Exam_22_Aug_2020-Retake/unit_testing/project/test/test.py
@@ -3,125 +3,74 @@ from project.student_report_card import StudentReportCard
 
 
 class StudentReportCardTests(TestCase):
-    def setUp(self):
-        self.student_report_card = StudentReportCard('Ivan', 10)
+    def setUp(self) -> None:
+        self.student_report_card = StudentReportCard("Joro", 12)
 
-    def test_init(self):
-        new_student_report_card = StudentReportCard('Gosho', 12)
-        # student_name, school_year, grades_by_subject
-        expected_student_name = 'Gosho'
-        expected_school_year = 12
-        expected_grades_by_subject = {}
-        self.assertEqual(expected_student_name, new_student_report_card.student_name)
-        self.assertEqual(expected_school_year, new_student_report_card.school_year)
-        self.assertEqual(expected_grades_by_subject, new_student_report_card.grades_by_subject)
+    def test_successful_initialization(self):
+        self.assertEqual("Joro", self.student_report_card.student_name)
+        self.assertEqual(12, self.student_report_card.school_year)
+        self.assertEqual({}, self.student_report_card.grades_by_subject)
 
-    def test_get_student_name(self):
-        expected_student_name = 'Ivan'
-        self.assertEqual(expected_student_name, self.student_report_card.student_name)
+    def test_unsuccessful_student_name_setter_raise_value_error_empty_string(self):
+        with self.assertRaises(ValueError) as ve:
+            self.student_report_card.student_name = ""
 
-    def test_set_student_name_valid(self):
-        expected_student_name = 'Ivan'
-        self.assertEqual(expected_student_name, self.student_report_card.student_name)
+        self.assertEqual("Student Name cannot be an empty string!", str(ve.exception))
 
-        self.student_report_card.student_name = 'Ivo'
-        expected_student_name_post = 'Ivo'
-        self.assertEqual(expected_student_name_post, self.student_report_card.student_name)
+    def test_successful_year(self):
+        self.student_report_card.school_year = 1
 
-    def test_set_student_name_invalid_should_raise(self):
-        with self.assertRaises(ValueError) as ex:
-            self.student_report_card.student_name = ''
+        self.assertEqual(1, self.student_report_card.school_year)
 
-        expected_assert_raises_message = 'Student Name cannot be an empty string!'
-        self.assertEqual(expected_assert_raises_message, str(ex.exception))
-
-    def test_get_school_year_valid(self):
-        expected_school_year = 10
-        self.assertEqual(expected_school_year, self.student_report_card.school_year)
-
-    def test_set_school_year_valid(self):
-        expected_school_year = 10
-        self.assertEqual(expected_school_year, self.student_report_card.school_year)
-
-        self.student_report_card.school_year = 11
-        expected_school_year_post = 11
-        self.assertEqual(expected_school_year_post, self.student_report_card.school_year)
-
-    def test_set_school_year_invalid_should_raise(self):
-        expected_assert_raises_message = 'School Year must be between 1 and 12!'
-
-        with self.assertRaises(ValueError) as ex_1:
+    def test_unsuccessful_school_year_setter_raise_value_error_less_than_1_and_more_than_12(self):
+        with self.assertRaises(ValueError) as ve:
             self.student_report_card.school_year = 13
-        self.assertEqual(expected_assert_raises_message, str(ex_1.exception))
 
-        with self.assertRaises(ValueError) as ex_2:
-            self.student_report_card.school_year = 0
-        self.assertEqual(expected_assert_raises_message, str(ex_2.exception))
+        self.assertEqual("School Year must be between 1 and 12!", str(ve.exception))
 
-        with self.assertRaises(ValueError) as ex_3:
-            self.student_report_card.school_year = -11
-        self.assertEqual(expected_assert_raises_message, str(ex_3.exception))
+    def test_successful_add_grade(self):
+        self.student_report_card.add_grade("Python", 6.00)
 
-        def test_add_grade_non_exising(self):
-            subject = 'Math'
-            self.assertNotIn(subject, self.student_report_card.grades_by_subject)
+        self.assertEqual({"Python": [6.00]}, self.student_report_card.grades_by_subject)
+        self.student_report_card.add_grade("Python", 6.00)
+        self.assertEqual({"Python": [6.00, 6.00]}, self.student_report_card.grades_by_subject)
 
-            self.student_report_card.add_grade(subject, 6)
-            self.assertIn(subject, self.student_report_card.grades_by_subject)
-            self.assertEqual([6], self.student_report_card.grades_by_subject[subject])
+    def test_successful_average_grade_by_subject(self):
+        self.student_report_card.add_grade("P - Basic", 6.00)
+        self.student_report_card.add_grade("P - Basic", 6.00)
+        self.student_report_card.add_grade("P - Fund", 6.00)
+        result = self.student_report_card.average_grade_by_subject()
 
-    def test_add_grade_existing(self):
-        subject = 'Math'
-        self.assertNotIn(subject, self.student_report_card.grades_by_subject)
+        self.assertEqual([6.0, 6.0], self.student_report_card.grades_by_subject["P - Basic"])
+        self.assertEqual([6.0], self.student_report_card.grades_by_subject["P - Fund"])
+        self.assertEqual(1, len(self.student_report_card.grades_by_subject["P - Fund"]))
+        self.assertEqual("P - Basic: 6.00\n"
+                         "P - Fund: 6.00", result)
 
-        self.student_report_card.add_grade(subject, 6)
-        self.assertIn(subject, self.student_report_card.grades_by_subject)
-        self.assertEqual([6], self.student_report_card.grades_by_subject[subject])
+    def test_successful_average_grade_for_all_subjects(self):
+        self.student_report_card.add_grade("P - Basic", 6.00)
+        self.student_report_card.add_grade("P - Basic", 6.00)
+        self.student_report_card.add_grade("P - Fund", 6.00)
 
-        self.student_report_card.add_grade(subject, 5)
-        self.assertEqual([6, 5], self.student_report_card.grades_by_subject[subject])
+        result = self.student_report_card.average_grade_for_all_subjects()
 
-    def test_average_grade_by_subject(self):
-        subject = 'Math'
-        self.assertNotIn(subject, self.student_report_card.grades_by_subject)
+        self.assertEqual("Average Grade: 6.00", result)
+        self.assertEqual([6.0], self.student_report_card.grades_by_subject["P - Fund"])
+        self.assertEqual([6.0, 6.0], self.student_report_card.grades_by_subject["P - Basic"])
 
-        self.student_report_card.add_grade(subject, 6)
-        self.assertIn(subject, self.student_report_card.grades_by_subject)
-        self.assertEqual([6], self.student_report_card.grades_by_subject[subject])
+    def test__repr__(self):
+        self.student_report_card.add_grade("P - Basic", 6.00)
+        self.student_report_card.add_grade("P - Basic", 6.00)
+        self.student_report_card.add_grade("P - Fund", 6.00)
 
-        self.student_report_card.add_grade(subject, 5)
-        self.assertEqual([6, 5], self.student_report_card.grades_by_subject[subject])
+        result = self.student_report_card.__repr__()
 
-        expected_result = 'Math: 5.50'
-        self.assertEqual(expected_result, self.student_report_card.average_grade_by_subject())
-
-    def test_average_grade_for_all_subjects(self):
-        subject = 'Math'
-        self.assertNotIn(subject, self.student_report_card.grades_by_subject)
-
-        self.student_report_card.add_grade(subject, 6)
-        self.assertIn(subject, self.student_report_card.grades_by_subject)
-        self.assertEqual([6], self.student_report_card.grades_by_subject[subject])
-
-        self.student_report_card.add_grade(subject, 5)
-        self.assertEqual([6, 5], self.student_report_card.grades_by_subject[subject])
-
-        expected_result = 'Average Grade: 5.50'
-        self.assertEqual(expected_result, self.student_report_card.average_grade_for_all_subjects())
-
-    def test_repr(self):
-        subject = 'Math'
-        self.assertNotIn(subject, self.student_report_card.grades_by_subject)
-
-        self.student_report_card.add_grade(subject, 6)
-        self.assertIn(subject, self.student_report_card.grades_by_subject)
-        self.assertEqual([6], self.student_report_card.grades_by_subject[subject])
-
-        self.student_report_card.add_grade(subject, 5)
-        self.assertEqual([6, 5], self.student_report_card.grades_by_subject[subject])
-
-        expected_result = "Name: Ivan\nYear: 10\n----------\nMath: 5.50\n----------\nAverage Grade: 5.50"
-        self.assertEqual(expected_result, self.student_report_card.__repr__())
+        self.assertEqual("Name: Joro\n"
+                         "Year: 12\n"
+                         "----------\n"
+                         f"{self.student_report_card.average_grade_by_subject()}\n"
+                         "----------\n"
+                         f"{self.student_report_card.average_grade_for_all_subjects()}", result)
 
 
 if __name__ == '__main__':
